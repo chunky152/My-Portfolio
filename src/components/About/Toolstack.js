8cfd85f5be975bd2314d612795e9629c2b17fb69
Muwanguzi Alvin Kiggundu <alvinmuwanguzi1@gmail.com>
2026-07-02 11:04:14 +0300

    feat: upgrade about section for platform engineering focus

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
-import linux from "../../Assets/TechIcons/Linux.svg";
+import postman from "../../Assets/TechIcons/Postman.svg";
+import macos from "../../Assets/TechIcons/Apple MacOSX.svg";
+
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={linux} alt="linux" className="tech-icon-images" />
-        <div className="tech-icons-text">Linux</div>
+        <img src={macos} alt="macOS" className="tech-icon-images" />
+        <div className="tech-icons-text">macOS</div>
       </Col>
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={chrome} alt="Chrome" className="tech-icon-images" />
-        <div className="tech-icons-text">Google Chrome</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons ">
+      <Col xs={4} md={2} className="tech-icons">
         <img src={vsCode} alt="vsCode" className="tech-icon-images" />
-        <div className="tech-icons-text">Vs Code</div>
+        <div className="tech-icons-text">VS Code</div>
       </Col>
-
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={postman} alt="Postman" className="tech-icon-images" />
+        <div className="tech-icons-text">Postman</div>
+      </Col>
     </Row>
   );
 }
