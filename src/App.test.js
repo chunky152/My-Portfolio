bd94d0ea0228094f689183f3326aa823b55462f3
Muwanguzi Alvin Kiggundu <alvinmuwanguzi1@gmail.com>
2026-07-03 08:32:55 +0300

    Fix CI test setup and add homepage for build

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('react-tsparticles', () => {
+  const React = require('react');
+  return {
+    __esModule: true,
+    default: (props) => React.createElement('div', props, null),
+    Particles: (props) => React.createElement('div', props, null),
+  };
+});
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
 });
